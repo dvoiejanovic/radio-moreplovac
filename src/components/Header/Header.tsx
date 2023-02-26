753e08c44c11c79be625a54de6186dc8952e7e4f
Ivo Dejanovic <ivo.dejanovic@productive.io>
2023-02-26 17:36:51 +0100

    nav buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from './Header.module.scss';
 import UserBadge from '../UserBadge/UserBadge';
 import {IUserProfile} from '../../services/spotify';
+import {AiOutlineLeft, AiOutlineRight} from 'react-icons/ai';
 
 interface IHeaderProps {
   userProfile: IUserProfile
@@ -9,8 +10,19 @@ interface IHeaderProps {
 const Header = (props: IHeaderProps) => {
   return (
     <header className={styles.header}>
-      <div>
-        // TODO: Navigation buttons
+      <div className={styles.nav_button_wrapper}>
+        <button
+          type="button"
+          className={styles.nav_button}
+        >
+          <AiOutlineLeft />
+        </button>
+        <button
+          type="button"
+          className={styles.nav_button}
+        >
+          <AiOutlineRight />
+        </button>
       </div>
       <UserBadge userProfile={props.userProfile} />
     </header>
