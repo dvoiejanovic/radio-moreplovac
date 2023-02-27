d5a2ad3a44246b0194f840ac82f9bc8613f02af5
Ivo Dejanovic <ivo.dejanovic@productive.io>
2023-02-27 22:44:48 +0100

    greeting

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -1,3 +1,18 @@
 export const formatNumber = (number: number): string => {
     return number?.toLocaleString();
 }
+
+export const formatTimeOfDay = (): string => {
+    const now = new Date();
+    const h = now.getHours();
+
+    if (h < 5) {
+        return 'night'
+    } else if (h < 12) {
+        return 'morning'
+    } else if (h < 19) {
+        return 'afternoon'
+    }
+
+    return 'evening';
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import {useEffect, useState} from "react";
 import Card from "~/components/Card";
 import CardGrid from "~/components/CardGrid";
+import {formatTimeOfDay} from "~/helpers/format";
 import {getUserTopArtists, ITopArtist} from "~/services/spotify";
 import styles from './styles.module.scss';
 
 const Home = () => {
   const [topArtists, setTopArtists] = useState<ITopArtist[]>([]);
+  const timeOfDay = formatTimeOfDay();
+
 
   useEffect(() => {
     const fetchTopArtists = async() => {
@@ -17,6 +20,9 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
+      <h2 className={styles.greeting}>
+        Good {timeOfDay}
+      </h2>
       <CardGrid>
         {
           topArtists.map((artist) => (
