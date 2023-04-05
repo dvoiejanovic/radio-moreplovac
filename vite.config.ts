bb0c2529a365d2841d8b2758721530d2f4043942
Ivo Dejanovic <ivo.dejanovic@productive.io>
2023-04-05 19:43:39 +0200

    home padding

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/radio-moreplovac/',
+  base: '',
   plugins: [react()],
   resolve: {
     alias: {
